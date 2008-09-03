dfb8858e3304bd5ff29c1f9250305384a31bbad2
SHIMODA Hiroshi <shimoda@clear-code.com>
2008-09-03 06:46:20 +0000

    UIと設定の対応

--- content/retentionsettingcontroller/configOverlay.js
+++ content/retentionsettingcontroller/configOverlay.js
@@ -13,6 +13,16 @@ var RetentionSettingControllerUI = {
 		return document.getElementById('tabpanels');
 	},
 
+	get selectedTab()
+	{
+		return this.tabContainer.selectedItem;
+	},
+	set selectedTab(aValue)
+	{
+		this.tabContainer.selectedItem = aValue;
+		return aValue;
+	},
+
 	createNewContents : function()
 	{
 		var range = document.createRange();
@@ -51,12 +61,76 @@ var RetentionSettingControllerUI = {
 
 	removeTab : function(aTab)
 	{
+		var panel = this.getPanelFromTab(aTab);
+		this.tabContainer.removeChild(aNode);
+		this.panelContainer.removeChild(panel);
+	},
+
+	getPanelFromTab : function(aTab)
+	{
+		var panel;
 		Array.slice(this.tabContainer.childNodes).some(function(aNode, aIndex) {
 			if (aNode != aTab) return false;
-			this.tabContainer.removeChild(aNode);
-			this.panelContainer.removeChild(this.panelContainer.childNodes[aIndex]);
+			panel = this.panelContainer.childNodes[aIndex];
 			return true;
 		}, this);
-	}
+		return panel;
+	},
 
+	getItemForSetting : function(aPanel, aName)
+	{
+		return aPanel.getElementsByAttribute('prefid', aName)[0];
+	},
+
+	setSettingsToTab : function(aTab, aSettings)
+	{
+		var panel = this.getPanelFromTab(aTab);
+		this.getItemForSetting(panel, 'pattern').value = aSettings.pattern;
+		this.getItemForSetting(panel, 'retainByPreference').value = aSettings.retainByPreference;
+		this.getItemForSetting(panel, 'numHeadersToKeep').value = aSettings.numHeadersToKeep;
+		this.getItemForSetting(panel, 'daysToKeepHdrs').value = aSettings.daysToKeepHdrs;
+		this.getItemForSetting(panel, 'keepUnreadMessagesOnly').checked = aSettings.keepUnreadMessagesOnly;
+	},
+
+	getSettingsFromTab : function(aTab)
+	{
+		var panel = this.getPanelFromTab(aTab);
+		return {
+			pattern : this.getItemForSetting(panel, 'pattern').value,
+			retainByPreference : parseInt(this.getItemForSetting(panel, 'retainByPreference').value),
+			numHeadersToKeep : parseInt(this.getItemForSetting(panel, 'numHeadersToKeep').value),
+			daysToKeepHdrs : parseInt(this.getItemForSetting(panel, 'daysToKeepHdrs').value),
+			keepUnreadMessagesOnly : this.getItemForSetting(panel, 'keepUnreadMessagesOnly').checked
+		};
+	},
+
+	initUIFromSettings : function(aSettingsArray)
+	{
+		try {
+			aSettingsArray = eval(aSettingsArray);
+		}
+		catch(e) {
+			aSettingsArray = [];
+		}
+		if (!aSettingsArray.length) {
+			this.addTab();
+		}
+		else {
+			var tabs = aSettingsArray.map(function(aSettings) {
+					var tab = this.addTab();
+					this.setSettingToTab(tab, aSettigns);
+					return tab;
+				}, this);
+			this.selectedTab = tabs[0];
+		}
+	},
+
+	getSettings : function()
+	{
+		var settingsArray = Array.splice(this.tabContainer.childNodes)
+				.map(function(aTab) {
+					return this.getSettingsFromTab(aTab);
+				}, this);
+		return settingsArray.toSource();
+	}
 };
